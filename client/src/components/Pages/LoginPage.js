02d488a800c7f9a6857da57bf6ed3970afede429
brendan-c <brendan.k.cooney@gmail.com>
2019-10-10 07:48:22 -0400

    CSS tweaks: forms, buttons, cards

--- client/src/components/Pages/LoginPage.js
+++ client/src/components/Pages/LoginPage.js
@@ -1,85 +1,100 @@
 // http://jasonwatmore.com/post/2018/09/11/react-basic-http-authentication-tutorial-example
 
-import React from 'react';
-import { Link, Redirect } from 'react-router-dom';
-import UserContext from '../Context/ContextProvider';
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import './LoginPage.css';
-
+import React from 'react'
+import { Link, Redirect } from 'react-router-dom'
+import UserContext from '../Context/ContextProvider'
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import './LoginPage.css'
 
 class LoginPage extends React.Component {
   constructor(props) {
-      super(props);
+    super(props)
 
-      this.state = {
-          username: '',
-          password: '',
-          submitted: false,
-          loading: false,
-          error: ''
-      };
+    this.state = {
+      username: '',
+      password: '',
+      submitted: false,
+      loading: false,
+      error: ''
+    }
 
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   handleChange(e) {
-      const { name, value } = e.target;
-      this.setState({ [name]: value });
+    const { name, value } = e.target
+    this.setState({ [name]: value })
   }
 
   handleSubmit(e) {
-      e.preventDefault();
+    e.preventDefault()
 
-      this.setState({ submitted: true });
-      const { username, password } = this.state;
+    this.setState({ submitted: true })
+    const { username, password } = this.state
 
-      // stop here if form is invalid
-      if (!(username && password)) {
-          return;
-      }
+    // stop here if form is invalid
+    if (!(username && password)) {
+      return
+    }
 
-      this.setState({ loading: true });
-      this.context.login(username, password);
+    this.setState({ loading: true })
+    this.context.login(username, password)
   }
 
   render() {
-      const { username, password, submitted, loading, error } = this.state;
-      if (this.context.isLoggedIn) {
-        return <Redirect to="/" />
-      }
-      return (
-          <div className="loginPage">
-              <h2 className="pageTitle">Login</h2>
-              <form name="form" onSubmit={this.handleSubmit}>
-                  <div className={'form-group'}>
-                      <input type="text" className="form-control" name="username" placeholder="Username" value={username} onChange={this.handleChange} />
-                      {submitted && !username &&
-                          <div className="help-block">Username is required</div>
-                      }
-                  </div>
-                  <div className={'form-group'}>
-                      <input type="password" className="form-control" name="password" placeholder="Password"  value={password} onChange={this.handleChange} />
-                      {submitted && !password &&
-                          <div className="help-block">Password is required</div>
-                      }
-                  </div>
-                  <div className="form-group">
-                      <button className="btn btn-primary" disabled={loading}>Login</button>
-                      {loading &&
-                          <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
-                      }
-                  </div>
-                  <Link to="/create_account">Don't have an account yet? Sign up here!</Link>
-                  {error &&
-                      <div className={'alert alert-danger'}>{error}</div>
-                  }
-              </form>
+    const { username, password, submitted, loading, error } = this.state
+    if (this.context.isLoggedIn) {
+      return <Redirect to="/" />
+    }
+    return (
+      <div className="loginPage">
+        <h2 className="pageTitle">Login</h2>
+        <form name="form" onSubmit={this.handleSubmit}>
+          <div className={'form-group'}>
+            <input
+              type="text"
+              className="form-control"
+              name="username"
+              placeholder="Username"
+              value={username}
+              onChange={this.handleChange}
+            />
+            {submitted && !username && (
+              <div className="help-block">Username is required</div>
+            )}
           </div>
-      );
+          <div className={'form-group'}>
+            <input
+              type="password"
+              className="form-control"
+              name="password"
+              placeholder="Password"
+              value={password}
+              onChange={this.handleChange}
+            />
+            {submitted && !password && (
+              <div className="help-block">Password is required</div>
+            )}
+          </div>
+          <div className="form-group">
+            <button className="btn btn-primary" disabled={loading}>
+              Login
+            </button>
+            {loading && (
+              <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
+            )}
+          </div>
+          <Link to="/create_account">
+            Don't have an account yet? Sign up here!
+          </Link>
+          {error && <div className={'alert alert-danger'}>{error}</div>}
+        </form>
+      </div>
+    )
   }
 }
-LoginPage.contextType = UserContext;
+LoginPage.contextType = UserContext
 
-export default LoginPage;
+export default LoginPage
